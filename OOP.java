62cbbfb5eef95a175e3b44d96b223e5f03de0c4d
Ujjwal Kumar <164533721+Ujjwal2023kumar@users.noreply.github.com>
2025-04-03 19:30:11 +0530

    Add files via upload

---
+++ OOP.java
@@ -0,0 +1,31 @@
+package OOPs;
+
+public class OOP {
+    public static void main(String[] args) {
+        Pen p1 = new Pen(); //created a pen object called p1
+        p1.setColor("Blue");
+        p1.setTip(5);
+        System.out.println(p1.getColor());
+        System.out.println(p1.getTip());
+
+    }
+}
+
+class Pen{
+    private String color;
+    private int tip;
+
+    String getColor(){
+        return this.color;
+    }
+    int getTip(){
+        return this.tip;
+    }
+    void setColor(String newColor){
+        this.color = newColor;
+    }
+    void setTip(int newTip){
+        this.tip = newTip;
+    }
+}
+
