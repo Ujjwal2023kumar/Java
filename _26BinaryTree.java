cf560f47189fe51b7cf9cbaa7e5534cfad71cd80
Ujjwal Kumar <164533721+Ujjwal2023kumar@users.noreply.github.com>
2025-08-10 20:25:49 +0530

    Update _26BinaryTree.java

--- _26BinaryTree.java
+++ _26BinaryTree.java
@@ -284,8 +284,8 @@ public class _26BinaryTree {
     }
     public static int minDist(Node root, int n1, int n2){
         Node lca = lca2(root, n1, n2);
-        int dist1 = lcaDist(root, n1);
-        int dist2 = lcaDist(root, n2);
+        int dist1 = lcaDist(lca, n1);
+        int dist2 = lcaDist(lca, n2);
         return dist1 + dist2;
     }
 
