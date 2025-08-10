35cc5edcaedc35204caabeb1d4e19f57200ac351
Ujjwal Kumar <164533721+Ujjwal2023kumar@users.noreply.github.com>
2025-08-10 10:19:31 +0530

    Update _27BST.java

--- _27BST.java
+++ _27BST.java
@@ -1,5 +1,19 @@
 public class _27BST {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data = data;
+        }
+    }
+    public static Node insert(){
+        
+    }
     public static void main(String[] args) {
         //Binary Search Tree
+        int values[] = {5,1,3,4,2,7};
+        Node root = null;
     }
 }
