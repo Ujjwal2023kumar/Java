a87dd56a43f710c1179d55a55b5216fdfa9bdec3
Ujjwal Kumar <164533721+Ujjwal2023kumar@users.noreply.github.com>
2025-08-26 20:16:05 +0530

    Update _04Sorting.java

--- _04Sorting.java
+++ _04Sorting.java
@@ -9,8 +9,7 @@ public class _04Sorting {
                     arr[j+1] = temp;
                 }
             }
-        }
-        
+        } 
     }
     public static void selectionSort(int arr[]){
         for(int i=0; i<arr.length-1; i++){
@@ -58,6 +57,60 @@ public class _04Sorting {
             }
         }
     }
+    public static void mergeSort(int arr[], int si, int ei){
+        if(si >= ei){
+            return;
+        }
+        int mid = si + (ei - si)/2;
+        mergeSort(arr, si, mid); // left part
+        mergeSort(arr, mid+1, ei); // right part
+        merge(arr, si, mid, ei);
+    }
+    public static void merge(int arr[], int si, int mid, int ei) {
+        int n1 = mid - si + 1;
+        int n2 = ei - mid;
+        int left[] = new int[n1];
+        int right[] = new int[n2];
+    
+        System.arraycopy(arr, si, left, 0, n1);
+        System.arraycopy(arr, mid + 1, right, 0, n2);
+    
+        int i = 0, j = 0, k = si;
+        while (i < n1 && j < n2) {
+            arr[k++] = (left[i] <= right[j]) ? left[i++] : right[j++];
+        }
+        
+        while (i < n1) arr[k++] = left[i++];
+        while (j < n2) arr[k++] = right[j++];
+    }
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(arr, low, high);
+            
+            quickSort(arr, low, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, high);
+        }
+    }
+    public static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1; 
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
     public static void printArr(int arr[]){
         for(int i=0;i<arr.length;i++){
             System.out.print(arr[i] + " ");
@@ -75,8 +128,7 @@ public class _04Sorting {
         printArr(arr);
         countingSort(arr);
         printArr(arr);
-        // We can use inbuilt sort Arrays.sort(arr, si, ei) 
-        Arrays.sort(arr); //O(n log n)
+        Arrays.sort(arr); 
         printArr(arr);
         // to get in reverse order
         Arrays.sort(arr2, Collections.reverseOrder());
@@ -84,6 +136,9 @@ public class _04Sorting {
             System.out.print(arr2[i] + " ");
         }
         System.out.println();
+        mergeSort(arr, 0, arr.length-1);
+        printArr(arr);
+        quickSort(arr, 0, arr.length - 1);
+        printArr(arr);
     }
 }
-
